d30663aa86eee405c176a5519416491d9aca0378
MR.ARIS <70155682+ariszywlafa1@users.noreply.github.com>
2020-09-07 15:34:08 +0700

    Update code.php

--- code.php
+++ code.php
@@ -2,7 +2,6 @@
         // Print out main menu..
     system('clear');
     system('figlet recode mati');
-    system('echo amin.....aowkaowk');
     echo "=========================\n";
     echo "=      TOOLS-ENCODE     =\n";
     echo "=     AUTHOR :MR.ARIS   =\n";
@@ -22,38 +21,46 @@
     echo "    3) Encode Sha1\n";
     echo "    4) Encode Md4\n";
     echo "    5) Encode Semua type\n";
+    echo "    6) Install bahan
     echo "    x) Exit\n";
     echo "Pilih Type Encode : ";
     switch(trim(fgets(STDIN,256)))
         {
             case 1:
-                echo "Masukkan Kata Yang Akan di Encode : ";
+                echo "Pilih Nomor Mana : ";
                 $kata = trim(fgets(STDIN,256));
                 echo "Encode Md5 :",md5($kata),"\n";
                 exit();
             case 2:
-                echo "Masukkan Kata Yang Akan di Encode : ";
+                echo "Pilih Nomor Mana : ";
                 $kata = trim(fgets(STDIN));
                 echo "Encode Base64 :",base64_encode($kata),"\n";
                 exit();
             case 3:
-                echo "Masukkan Kata Yang Akan di Encode : ";
+                echo "Pilih Nomor Mana : ";
                 $kata = trim(fgets(STDIN));
                 echo "Encode Base64 :",sha1($kata),"\n";
                 exit();
             case 4:
-                echo "Masukkan Kata Yang Akan di Encode : ";
+                echo "Pilih Nomor Mana : ";
                 $kata = trim(fgets(STDIN));
                 echo "Encode Md4 :",crypt('md4',$kata);
                 exit();
             case 5:
-                echo "Masukan Kata Yang Akan di Encode : ";
+                echo "Pilih Nomor Mana : ";
                 $kata = trim(fgets(STDIN));
                 foreach (hash_algos() as $v) {
                 $r = hash($v, $kata, false);
                 printf("%-12s %3d %s\n", $v, strlen($r), $r);
                 }
                 exit();
+            case 6:
+                echo "Pilih Nomor Mana : ";
+                system('pkg update && pkg upgrade');
+                system('pkg install git');
+                echo "sudah gan";
+                exit();
+    
             case "x":
                 exit();
         }
